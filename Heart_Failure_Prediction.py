b68aed3f6d5cd9dfb5d721f5781d6d6f2439f42a
Haroon Mahmood <148611250+haroonaulakh@users.noreply.github.com>
2024-12-25 23:29:02 +0500

    App Deployed2

--- Heart_Failure_Prediction.py
+++ Heart_Failure_Prediction.py
@@ -8,28 +8,26 @@ import base64
 # title 
 st.title("Heart Failure Prediction")
 
-# background
-# Function to encode a local image
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+uploaded_file = st.file_uploader("https://www.magicpattern.design/tools/starry-sky-generator", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+    img_path = uploaded_file.name
+    with open(img_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    base64_img = get_base64_of_bin_file(img_path)
 
-# Path to your local image
-img_path = "steptodown.com601822.jpg"
-base64_img = get_base64_of_bin_file(img_path)
+    # Custom CSS for background image
+    page_bg_img = f"""
+    <style>
+    body {{
+        background-image: url("data:image/jpeg;base64,{base64_img}");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    </style>
+    """
+    st.markdown(page_bg_img, unsafe_allow_html=True)
 
-# Custom CSS for background image
-page_bg_img = f"""
-<style>
-body {{
-    background-image: url("data:image/jpeg;base64,{base64_img}");
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-}}
-</style>
-"""
 
 # Apply the CSS
 st.markdown(page_bg_img, unsafe_allow_html=True)
