e8f4737f5b40491d6d23badb4584c96419144c2c
Haroon Mahmood <148611250+haroonaulakh@users.noreply.github.com>
2024-12-26 00:01:18 +0500

    code changing

--- Heart_Failure_Prediction.py
+++ Heart_Failure_Prediction.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="Heart Failure Prediction"
-    page_icon="https://www.flaticon.com/free-icon/heart_508735?term=heart&page=1&position=12&origin=tag&related_id=508735"
-    layout= "wide"
+    page_title="Heart Failure Prediction",
+    page_icon="https://www.flaticon.com/free-icon/heart_508735?term=heart&page=1&position=12&origin=tag&related_id=508735",
+    layout= "wide",
     initial_sidebar_state="expanded"
     )
